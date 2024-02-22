8e9eb21115e972d7645ef9c91a61abfebbdecda2
Bikash <sahb2676@gmail.com>
2024-02-22 18:22:06 +0545

    03-Typography

---
+++ 03-Typography/src/App.jsx
@@ -0,0 +1,40 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl italic leading-loose underline decoration-red-600 decoration-wavy underline-offset-4">
+        Title-1
+      </h1>
+      <h2 className="text-2xl">Title-2</h2>
+      <h3 className="text-xl">Title-3</h3>
+      <p className="text-base overline">A regular paragraph</p>
+      <p className="text-sm">A Description paragraph</p>
+      <p className="text-xs">A note is here</p>
+      <p className="truncate w-1/2">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint facere
+        nobis, a sapiente delectus consequuntur ipsa possimus sequi est laborum
+        provident eius temporibus natus reprehenderit iure deserunt?
+        Reprehenderit eius incidunt rerum maiores labore numquam nisi quibusdam
+        quo cumque quaerat repellat saepe sequi, tempore quidem veritatis ipsa
+        omnis debitis vel iusto quam inventore molestias. Explicabo, ipsam sed!
+      </p>
+      <p className="whitespace-nowrap">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+        molestias ratione minus perspiciatis unde, obcaecati eos, modi quo
+        quidem, repellendus ullam. Nulla quis porro earum iusto facilis nemo ex
+        numquam inventore fugit quae eveniet voluptate harum quasi ipsa, magni
+        nam obcaecati labore aliquam. Nemo, perspiciatis, atque architecto illum
+        nobis provident, officiis quas sint voluptate magni dolorum jbsjbjc mn
+        bkwjab sc bkjwsbkcj bkjsv kjwbasckb aksbcl besd kvjb wes,dbv le,af
+        clqekabs kzcj m?
+      </p>
+      <p className="break-words">
+        vfehhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+        bbaskjbkjasbkjbajfkckegkfbekujavfckbeuafvcj
+      </p>
+    </>
+  );
+}
+
+export default App;
